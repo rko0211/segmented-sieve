18ec436b8b6bb81723dea609a15c2a1997255623
Prakash Mondal <2025.cse45.prakashmondal@gmail.com>
2023-02-10 22:57:41 +0530

    My first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # segmented-sieve
 i have provided the code of segmented sieve(sieve of eratosthenesis) in this repo.
+Hello World
--- segmented_sieve.cpp
+++ segmented_sieve.cpp
@@ -1,46 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int  n = 1000000;
-bool sieve[1000001];
+// int  n = 1000000;
+// bool sieve[1000001];
 
-void createseive()
+// void createseive()
+// {
+//     // O(nloglogn)
+
+//     for (int i = 2; i <= n; i++)
+//     {
+//         // Initially all are primes
+//         sieve[i] = true;
+//     }
+//     for (int i = 2; i * i <= n; i++)
+//     {
+//         if (sieve[i]==true)
+//         {
+//             for (int j = i * i; j <= n; j +=i)
+//             {
+//                 // Not prime number
+//                 sieve[i] = 0;
+//             }
+//         }
+//     }
+// }
+vector<int> primes;
+void findallprimes(int p)
 {
-    // O(nloglogn)
-
-    for (int i = 2; i <= n; i++)
+    vector<bool>isprimes(p+1,true);
+    
+    for (int i = 2; i <= p; i++)
     {
-        // Initially all are primes
-        sieve[i] = true;
-    }
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (sieve[i]==true)
+        if (isprimes[i]==true)
         {
-            for (int j = i * i; j <= n; j +=i)
+            primes.push_back(i);
+            for(int j=i*i;j<=p;j+=i)
             {
-                // Not prime number
-                sieve[i] = 0;
+                isprimes[j]=false;
             }
         }
     }
-}
-vector<int> findallprimes(int p)
-{
-    vector<int> allprimes;
-    for (int i = 2; i <= p; i++)
-    {
-        if (sieve[i]==true)
-        {
-            allprimes.push_back(i);
-        }
-    }
-    return allprimes;
+
 }
 
 int main()
 {
-    createseive();
+    // createseive();
 
     int t;
     cin >> t;
@@ -51,15 +57,11 @@ int main()
         //    Step 1
         //  We have to store all the primes present till sqrt(r);
 
-        vector<int> primes = findallprimes(sqrt(r));
+        findallprimes(sqrt(r));
         // Step 2
         // Creat a dummy array of size (r-l+1) and mark that with 1;
 
-        int dummy[r - l + 1];
-        for (int i = 0; i < (r - l + 1); i++)
-        {
-            dummy[i] = 1;
-        }
+      vector<bool>dummy(r-l+1,true);
 
         // Step 3
         // Mark all multiples of primes in l to r in dummy array;
@@ -74,13 +76,13 @@ int main()
             // We have iterate on each multiple of primes;
             for (int j = max(firstmultiples, pr * pr); j <= r; j += pr)
             {
-                dummy[j - l] = 0;
+                dummy[j - l] = false;
             }
         }
         // Step 4 get all the primes
         for (int i = l; i <= r; i++)
         {
-            if (dummy[i - l]==1)
+            if (dummy[i - l]==true)
             {
                 cout << i << " ";
             }
